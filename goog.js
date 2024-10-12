2b47431cd77c50c0441be5aca8ff8fff133b6b10
asafBH <34339265+asafBH@users.noreply.github.com>
2024-10-12 17:55:02 +0300

    Fix project by issues marks

--- goog.js
+++ goog.js
@@ -1,75 +1,54 @@
 function initMap() {
-  var options = {
+  const options = {
     zoom: 4,
     center: { lat: 42.4668, lng: -70.9495 }
   };
-  var map = new google.maps.Map(document.getElementById("map"), options);
+  const map = new google.maps.Map(document.querySelector("#map"), options);
 
-  //Add Marker Function
-  function addMarker(coords, wheatherDesc) {
-    var marker = new google.maps.Marker({
+  // Add Marker Function
+  const addMarker = (coords, weatherDesc) => {
+    const marker = new google.maps.Marker({
       position: coords,
       map: map,
       animation: google.maps.Animation.DROP
     });
-    var infowindow = new google.maps.InfoWindow({
-      content: wheatherDesc
+    const infowindow = new google.maps.InfoWindow({
+      content: weatherDesc
     });
-    marker.addListener("click", function() {
+    marker.addListener("click", () => {
       infowindow.open(map, marker);
     });
-  }
+  };
 
-  google.maps.event.addListener(map, "click", function(event) {
-    var latitude = event.latLng.lat();
-    var longitude = event.latLng.lng();
-    var url =
-      "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&APPID=c894fa26f98d61a6892f65d827493bef&units=metric";
+  google.maps.event.addListener(map, "click", (event) => {
+    const latitude = event.latLng.lat();
+    const longitude = event.latLng.lng();
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=c894fa26f98d61a6892f65d827493bef&units=metric`;
 
-    fetch(url, { method: "get" })
-      .then(function(response) {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok.");
         return response.json();
       })
-      .then(function(myjason) {
-        //var contentString = myjason;
-        var contentString =
-          '<div id="content">' +
-          '<div id="siteNotice">' +
-          "</div>" +
-          '<h1 id="firstHeading" class="firstHeading">Current weather for: ' +
-          myjason.name +
-          ", " +
-          myjason.sys.country +
-          "</h1>" +
-          '<div id="bodyContent">' +
-          "<p><b>Weather</b>: " +
-          myjason.weather[0].main +
-          "<img src=http://openweathermap.org/img/wn/" +
-          myjason.weather[0].icon +
-          ".png>" +
-          "<b><br>Description</b>: " +
-          myjason.weather[0].description +
-          "<b><br>Temperature</b>: " +
-          myjason.main.temp +
-          "&deg;C" +
-          "<b><br>Humidity</b>: " +
-          myjason.main.humidity +
-          "%" +
-          "<b><br>Wind Speed</b>: " +
-          myjason.wind.speed +
-          "m/s" +
-          "</p>" +
-          "</div>" +
-          "</div>";
-
+      .then((weatherData) => {
+        const contentString = `
+          <div id="content">
+            <h1 id="firstHeading" class="firstHeading">Current weather for: ${weatherData.name}, ${weatherData.sys.country}</h1>
+            <div id="bodyContent">
+              <p>
+                <b>Weather</b>: ${weatherData.weather[0].main} <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png">
+                <b><br>Description</b>: ${weatherData.weather[0].description}
+                <b><br>Temperature</b>: ${weatherData.main.temp}°C
+                <b><br>Humidity</b>: ${weatherData.main.humidity}%
+                <b><br>Wind Speed</b>: ${weatherData.wind.speed} m/s
+              </p>
+            </div>
+          </div>`;
         addMarker({ lat: latitude, lng: longitude }, contentString);
       })
-      .catch(function(err) {
-        // Error
+      .catch((err) => {
+        console.error("Error fetching weather data: ", err);
+        alert("Could not retrieve weather data. Please try again later.");
       });
   });
 }
